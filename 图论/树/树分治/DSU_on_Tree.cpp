d1c68e72651ed627bdf06c4f9d7de05df0b07ee6
Acathe <1353217816@qq.com>
2020-09-28 04:22:31 +0800

    添加 /图论/树/树分治

---
+++ 图论/树/树分治/DSU_on_Tree.cpp
@@ -0,0 +1,129 @@
+/**
+ * @birth: created by Acathe on 2020-09-28
+ * @content: 点分治
+ * @version 1.0.0
+ * @revision: last revised by Acathe on 2020-09-28
+ * @template: http://codeforces.com/problemset/problem/600/E
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const size_t kMaxV = 1e5 + 5;
+
+struct Edge { int u, v; };
+
+struct Graph {
+    int w[kMaxV];
+    vector<int> adj[kMaxV];
+    vector<Edge> e;
+
+    void init() {
+        e.clear();
+        memset(w, 0, sizeof(w));
+        for (auto& i: adj)
+            i.clear();
+    }
+
+    void addEdge(int u, int v) {
+        adj[u].push_back(e.size());
+        e.push_back({u, v});
+    }
+};
+
+struct DsuOnTree : Graph {
+    int siz[kMaxV], child[kMaxV];
+    int buc[kMaxV], maxx;
+    int64_t ans[kMaxV], res;
+
+    void addEdge(int u, int v) {
+        Graph::addEdge(u, v);
+        Graph::addEdge(v, u);
+    }
+
+    void dfsChild(int u, int par){
+        child[u] = 0;
+        siz[u] = 1;
+        for (const auto& i: adj[u]) {
+            int v = e[i].v;
+            if(v == par) continue;
+            dfsChild(v, u);
+            siz[u] += siz[v];
+            if(siz[child[u]] < siz[v]){
+                child[u] = v;
+            }
+        }
+    }
+
+    void modify(int k, int v) {
+        buc[k] += v;
+        if (maxx < buc[k]) {
+            maxx = buc[k];
+            res = k;
+        } else if (maxx == buc[k]) {
+            res += k;
+        }
+    }
+
+    void update(int u, int par, int val) {
+        modify(w[u], val);
+        for (const auto& i: adj[u]) {
+            int v = e[i].v;
+            if(v == par) continue;
+            update(v, u, val);
+        }
+    }
+
+    void dfs(int u, int par) {
+        for (const auto& i: adj[u]) {
+            int v = e[i].v;
+            if(v == par || v == child[u]) continue;
+            dfs(v, u);
+        }
+        if (child[u]) dfs(child[u], u);
+        modify(w[u], 1);
+        for (const auto& i: adj[u]) {
+            int v = e[i].v;
+            if(v == par || v == child[u]) continue;
+            update(v, u, 1);
+        }
+        ans[u] = res;
+        if (u != child[par]) {
+            update(u, par, -1);
+            maxx = res = 0;
+        }
+    }
+
+    void solveDsuOnTree() {
+        res = 0;
+        siz[0] = 0;
+        memset(buc, 0, sizeof(buc));
+        dfsChild(1, 0);
+        dfs(1, 0);
+    }
+};
+
+int n;
+DsuOnTree tree;
+
+int main() {
+#ifndef DEBUG
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif // DEBUG
+    cin >> n;
+    tree.init();
+    for (int i = 1; i <= n; ++i)
+        cin >> tree.w[i];
+    for (int i = 1; i < n; ++i) {
+        int u, v;
+        cin >> u >> v;
+        tree.addEdge(u, v);
+    }
+    tree.solveDsuOnTree();
+    for (int i = 1; i <= n; ++i)
+        cout << tree.ans[i] << ' ';
+    cout << '\n';
+    return 0;
+}
---
+++ 图论/树/树分治/点分治.cpp
@@ -0,0 +1,130 @@
+/**
+ * @birth: created by Acathe on 2020-09-28
+ * @content: 点分治
+ * @version 1.0.0
+ * @revision: last revised by Acathe on 2020-09-28
+ * @template: http://codeforces.com/problemset/problem/161/D
+*/
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+const size_t kMaxV = 1e5 + 5;
+const size_t kInf = 0x3f3f3f3f;
+
+struct Edge { int u, v, w; };
+
+struct Graph {
+    vector<int> adj[kMaxV];
+    vector<Edge> e;
+
+    void init() {
+        e.clear();
+        for (auto& i: adj)
+            i.clear();
+    }
+
+    void addEdge(int u, int v, int w) {
+        adj[u].push_back(e.size());
+        e.push_back({u, v, w});
+    }
+};
+
+struct TreeCent : Graph {
+    int n, siz[kMaxV];
+    int cent, dp[kMaxV];
+    int vis[kMaxV];
+
+    void dfs(int u, int par) {
+        siz[u] = 1;
+        dp[u] = 0;
+        for (const auto& i: adj[u]) {
+            int v = e[i].v;
+            if (vis[v] || v == par) continue;
+            dfs(v, u);
+            siz[u] += siz[v];
+            dp[u] = max(dp[u], siz[v]);
+        }
+        dp[u] = max(dp[u], n - siz[u]);
+        if (dp[u] < dp[cent]) cent = u;
+    }
+
+    int solveTreeCent(int u, int n) {
+        this->n = n;
+        cent = 0;
+        dfs(u, 0);
+        return cent;
+    }
+};
+
+struct TreeDivide : TreeCent {
+    int dist[kMaxV], cnt;
+    int k, ans;
+
+    void addEdge(int u, int v, int w) {
+        Graph::addEdge(u, v, w);
+        Graph::addEdge(v, u, w);
+    }
+
+    void dfsDist(int u, int par, int dep) {
+        dist[cnt++] = dep;
+        for (const auto& i: adj[u]) {
+            int v = e[i].v, w = e[i].w;
+            if (vis[v] || v == par) continue;
+            dfsDist(v, u, dep + w);
+        }
+    }
+
+    int calc(int u, int dep) {
+        cnt = 0;
+        dfsDist(u, 0, dep);
+        sort(dist, dist + cnt);
+        int res = 0;
+        for (int i = 0; i < cnt && dist[i] <= k - dist[i]; ++i) {
+            int l = lower_bound(dist + i + 1, dist + cnt, k - dist[i]) - dist;
+            int r = upper_bound(dist + i + 1, dist + cnt, k - dist[i]) - dist;
+            res += r - l;
+        }
+        return res;
+    }
+
+    void dfs(int u) {
+        vis[u] = true;
+        ans += calc(u, 0);
+        for (const auto& i: adj[u]) {
+            int v = e[i].v, w = e[i].w;
+            if (vis[v]) continue;
+            ans -= calc(v, w);
+            dfs(solveTreeCent(v, siz[v]));
+        }
+    }
+
+    int solveVertexDAC(int k, int n) {
+        this->k = k;
+        ans = 0;
+        dp[0] = kInf;
+        memset(vis, 0, sizeof(vis));
+        dfs(solveTreeCent(1, n));
+        return ans;
+    }
+};
+
+int n, k;
+TreeDivide tree;
+
+int main() {
+#ifndef DEBUG
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+#endif // DEBUG
+    cin >> n >> k;
+    tree.init();
+    for (int i = 1; i < n; ++i) {
+        int u, v;
+        cin >> u >> v;
+        tree.addEdge(u, v, 1);
+    }
+    cout << tree.solveVertexDAC(k, n) << '\n';
+    return 0;
+}
